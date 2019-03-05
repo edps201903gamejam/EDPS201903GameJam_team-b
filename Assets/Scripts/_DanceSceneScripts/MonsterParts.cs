26b00912e87317adde9ab410d2aefd902190a8aa
xakazukinx <xakazukinx.goo@gmail.com>
2019-03-05 16:37:10 +0900

    [add]ダンスシーン用の諸々作成。作業途中

---
+++ Assets/Scripts/_DanceSceneScripts/MonsterParts.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterParts
+{
+	public enum MONSTER_PARTS
+	{
+		//親オブジェクトから中心位置から
+		//LeftなのかRightなのかを判定して生成時に情報をアタッチしてあげてほしい。
+		LEFT_HAND,
+		RIGHT_HAND,
+		LEFT_FOOT,
+		RIGHT_FOOT,
+		LEFT_EYE,
+		RIGHT_EYE,
+		NOSE,
+		LEFT_BROWS,
+		RIGHT_BROWS,
+		BODY,
+		OTHER,
+	}
+}
---
+++ Assets/Scripts/_MonsterScripts/Parts.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parts : MonoBehaviour {
+
+	public MonsterParts partsType;
+	private Transform parentTransform;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//partsTypeaをここで設定する。
+	//座標から拾ってくるイメージ。
+	public void setPartsType()
+	{
+		if(transform.parent == null)
+		{
+			Debug.LogError("深刻なエラーが発生しました。Monsterの実態となるオブジェクトが存在していないみたいです。");
+			return;
+		}
+		else
+		{
+			parentTransform = transform.parent;
+		}
+	}
+
+	public void testGetParts()
+	{
+		Debug.Log(partsType);
+	}
+}
