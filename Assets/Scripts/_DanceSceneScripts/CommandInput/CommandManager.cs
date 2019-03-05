0767319d6a4c70dc799cef2a273af53ec3ae9598
xakazukinx <xakazukinx.goo@gmail.com>
2019-03-06 03:47:48 +0900

    [add]Commandを作成する部分を作成途中

---
+++ Assets/Scripts/_DanceSceneScripts/CommandInput/CommandManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DanceScene
+{
+	public class CommandManager : SingletonMonoBehaviour<CommandManager>
+	{
+
+		public KeyCode[] danceButton;
+		public int commandNum;
+		
+		// Use this for initialization
+		void Start () 
+		{
+		
+		}
+	
+		// Update is called once per frame
+		void Update () 
+		{
+			if (Input.GetKeyDown(KeyCode.T))
+			{
+				CommandGenerator commandGenerator = new CommandGenerator();
+				commandGenerator.GenerateCommand();
+				Debug.Log("Debug");
+			}
+		}
+	}	
+
+}
