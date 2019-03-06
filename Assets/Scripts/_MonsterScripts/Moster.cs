40f8c5de2f2a573e7533100428c95f70879f4a04
xakazukinx <xakazukinx.goo@gmail.com>
2019-03-07 04:17:07 +0900

    [add]Monster生成できそうなscript作成

--- Assets/Scripts/_MonsterScripts/Moster.cs
+++ Assets/Scripts/_MonsterScripts/Moster.cs
@@ -2,13 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using akazukin_omochabako;
 
 namespace Monster
 {
 	public class Moster : MonoBehaviour
 	{
-		//ここでMonsterを生成する処理
-		//こオブジェクトになっているはずのMonsterPartsをまとめて拾って格納。
+		private void Awake()
+		{
+/*
+			DontDestroyOnLoad(gameObject);
+*/
+		}
+
+		private void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.F1))
+			{
+				LoadScene.Instance.LoadStart();
+			}
+		}
+
+		public void generate()
+		{
+			DontDestroyOnLoad(gameObject);
+			LoadScene.Instance.LoadStart();
+		}
+
+		public void OnClick()
+		{
+			generate();
+		}
 	}	
 
 }
---
+++ Assets/Scripts/others/LoadScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace akazukin_omochabako
+{
+	public class LoadScene : SingletonMonoBehaviour<LoadScene>
+	{
+
+		[SerializeField] private string LoadSceneName;
+		
+		public void LoadStart()
+		{
+			SceneManager.LoadScene(LoadSceneName);
+		}
+		
+/*		public void LoadStart()
+		{
+			SceneManager.LoadScene(LoadSceneName);
+		}*/
+	}
+
+}
