11a2433eb7cb9556379ddfe4a3b5c25e2e0d3e10
xakazukinx <xakazukinx.goo@gmail.com>
2019-03-06 10:52:39 +0900

    [add]コマンド生成の仕組み作成

--- Assets/Scripts/_DanceSceneScripts/CommandInput/CommandManager.cs
+++ Assets/Scripts/_DanceSceneScripts/CommandInput/CommandManager.cs
@@ -1,13 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace DanceScene
 {
 	public class CommandManager : SingletonMonoBehaviour<CommandManager>
 	{
-
 		public KeyCode[] danceButton;
+		private KeyCode[] danceCommand;
+		
 		public int commandNum;
 		
 		// Use this for initialization
@@ -19,11 +22,23 @@ namespace DanceScene
 		// Update is called once per frame
 		void Update () 
 		{
+			//test
 			if (Input.GetKeyDown(KeyCode.T))
 			{
-				CommandGenerator commandGenerator = new CommandGenerator();
-				commandGenerator.GenerateCommand();
-				Debug.Log("Debug");
+				commandGenerate();
+			}
+		}
+
+
+		private void commandGenerate()
+		{
+			Array.Resize(ref danceCommand,commandNum);
+			for (int i = 0; i < commandNum; i++)
+			{
+				var keyNum = Random.Range(0, danceButton.Length);
+				Debug.Log(danceButton[keyNum]);
+				danceCommand[i] = danceButton[keyNum];
+				Debug.Log(danceCommand[i]);
 			}
 		}
 	}	
