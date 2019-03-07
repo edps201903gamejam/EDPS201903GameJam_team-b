7a385e9b5f99b307a7ad433be65496839b34cfbc
Nakasan971 <ne300031@senshu-u.jp>
2019-03-08 05:26:55 +0900

    少々修正

--- Assets/Scripts/MakingScripts/FirstSelect/BodyScript.cs
+++ Assets/Scripts/MakingScripts/FirstSelect/BodyScript.cs
@@ -12,6 +12,7 @@ public class BodyScript : MonoBehaviour {
 	public Button Parts2;
 	public Button Parts3;
 
+
 	public bool EyeTrigger = false;
 
 	int i = 0;
@@ -26,32 +27,29 @@ public class BodyScript : MonoBehaviour {
 
 	void Update()
 	{
-
 		if (EyeTrigger)
 		{
-			Parts1.transform.position = new Vector3(Set1.transform.position.x, Set1.transform.position.y, 1);
-			Parts2.transform.position = new Vector3(Set2.transform.position.x, Set2.transform.position.y, 1);
-			Parts3.transform.position = new Vector3(Set3.transform.position.x, Set3.transform.position.y, 1);
+			Parts1.transform.position = new Vector3(Set1.transform.position.x, Set1.transform.position.y, 0);
+			Parts2.transform.position = new Vector3(Set2.transform.position.x, Set2.transform.position.y, 0);
+			Parts3.transform.position = new Vector3(Set3.transform.position.x, Set3.transform.position.y, 0);
 		}
-
-		if (!EyeTrigger)
+		else if (!EyeTrigger)
 		{
-			Parts1.transform.position = new Vector3(-200, Parts1.transform.position.y, 0);
-			Parts2.transform.position = new Vector3(-200, Parts2.transform.position.y, 0);
-			Parts3.transform.position = new Vector3(-200, Parts3.transform.position.y, 0);
+			Parts1.transform.position = new Vector3(-100, Parts1.transform.position.y, 0);
+			Parts2.transform.position = new Vector3(-100, Parts2.transform.position.y, 0);
+			Parts3.transform.position = new Vector3(-100, Parts3.transform.position.y, 0);
 		}
 	}
-
-
 
 	public void OnClick()
 	{
 		i++;
-		if(i % 2 == 1){
+		if (i % 2 == 1)
+		{
+			Debug.Log("clicked");
 			EyeTrigger = true;
 		}else if(i % 2 == 0){
 			EyeTrigger = false;
 		}
 	}
-
 }
