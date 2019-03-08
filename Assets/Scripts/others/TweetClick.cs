db562511003c62b51a05c95b40e309706e053ade
xakazukinx <xakazukinx.goo@gmail.com>
2019-03-08 12:14:14 +0900

    [add] Tweet機能作った

---
+++ Assets/Scripts/others/TweetClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DanceScene;
+
+public class TweetClick : MonoBehaviour
+{
+    public void OnClick()
+    {
+		var tweetText = "あなたのライブの点数は"+ DanceSceneManager.CORRECT_COUNT*100 +"点！！！";
+		Application.OpenURL("http://twitter.com/intent/tweet?text=" + WWW.EscapeURL(tweetText + " #EDPS_201903_GameJam"));
+        //Application.OpenURL("https://twitter.com/intent/tweet?text="+tweetText+"&hashtags=EDPS_201903_GameJam,akazukin_game");
+    }
+}
